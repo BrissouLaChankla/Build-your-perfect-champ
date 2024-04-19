78f473ee7582fdcaf9764e6eaeb070e7fdf92e31
Brice Eliasse <brice.eliasse@gmail.com>
2024-04-19 14:53:42 +0200

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
-
 
 export const metadata = {
   title: "Legends' Fusion - Build your Lol Champ",
@@ -10,10 +10,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <>
+      <Analytics />
+      <html lang="fr" data-theme="dark">
 
-    <html lang="fr" data-theme="dark">
-     
-      <body className={inter.className} style={{ overflowX: "hidden" }} >{children}</body>
-    </html >
+        <body className={inter.className} style={{ overflowX: "hidden" }} >{children}</body>
+      </html >
+    </>
   );
 }
