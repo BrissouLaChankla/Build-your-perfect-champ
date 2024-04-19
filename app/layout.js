555e9fa1c19ebfa10135701efe894a24b76fa3a7
Brice Eliasse <brice.eliasse@gmail.com>
2024-04-19 12:24:14 +0200

    fix url meta og

--- app/layout.js
+++ app/layout.js
@@ -16,18 +16,18 @@ export default function RootLayout({ children }) {
         <title>Legends' Fusion - Build your Lol Champ</title>
         <meta name="description" content="Randomly select skills from League of legends champions and combine them to create a unique legend" />
 
-        <meta property="og:url" content="https://legends-fusion.com/" />
+        <meta property="og:url" content="https://www.legends-fusion.com/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Legends' Fusion - Build your Lol Champ" />
         <meta property="og:description" content="Randomly select skills from League of legends champions and combine them to create a unique legend" />
-        <meta property="og:image" content="https://legends-fusion.com/meta.png" />
+        <meta property="og:image" content="https://www.legends-fusion.com/meta.png" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="legends-fusion.com" />
-        <meta property="twitter:url" content="https://legends-fusion.com/" />
+        <meta property="twitter:url" content="https://www.legends-fusion.com/" />
         <meta name="twitter:title" content="Legends' Fusion - Build your Lol Champ" />
         <meta name="twitter:description" content="Randomly select skills from League of legends champions and combine them to create a unique legend" />
-        <meta name="twitter:image" content="https://legends-fusion.com/meta.png" />
+        <meta name="twitter:image" content="https://www.legends-fusion.com/meta.png" />
 
       </Head>
       <body className={inter.className} style={{ overflowX: "hidden" }} >{children}</body>
